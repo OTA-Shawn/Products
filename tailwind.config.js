f1690f49290801c571770f4e01fe93524c314dcf
OTA-Shawn <shawn.b@osakititan.com>
2024-01-04 10:49:39 -0600

    prefix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
   },
+  corePlugins: {
+    preflight: false,
+  },
   plugins: [],
+  prefix: "tw-",
 }
