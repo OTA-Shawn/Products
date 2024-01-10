b6f2c3cb024d0c85b2c91e4f47c82ff1b1fb514d
OTA-Shawn <shawn.b@osakititan.com>
2024-01-10 15:29:17 -0600

    ~Encore + utilfile

---
+++ utils/utils.js
@@ -0,0 +1,56 @@
+function createIntensityLevel(minIntensity, maxIntensity) {
+  // Create root element
+  const rootElement = document.getElementById("intensity-level")
+
+  // Create main container
+  const mainContainer = document.createElement("div")
+  mainContainer.style.display = "flex"
+  mainContainer.style.alignItems = "center"
+  mainContainer.style.gap = "0.5rem"
+
+  // Add Intensity Level text
+  mainContainer.textContent = `Intensity Level: ${minIntensity}~${maxIntensity}`
+
+  // Create tooltip container
+  const tooltipContainer = document.createElement("div")
+  tooltipContainer.style.position = "relative"
+
+  // Create help span
+  const helpSpan = document.createElement("span")
+  helpSpan.className = "material-symbols-outlined intensity-tooltip"
+  helpSpan.style.cursor = "pointer"
+  helpSpan.textContent = "help"
+
+  // Create intensity modal
+  const intensityModal = document.createElement("div")
+  intensityModal.className = "intensity-modal"
+
+  // Create image element in the modal
+  const modalImage = document.createElement("img")
+  modalImage.style.width = "300px"
+  modalImage.src =
+    "https://cdn.shopify.com/s/files/1/0579/8379/5374/files/Screenshot_2024-01-10_at_12.19.56.png?v=1704919961"
+  modalImage.alt = ""
+
+  // Create text content in the modal
+  const modalText = document.createElement("div")
+  modalText.innerHTML = `
+  The Intensity Level represents both Minimum Intensity and Maximum Intensity.
+  <br />
+  <b>Note:</b> Intensity level is a relative indicator that can be perceived differently by each user.
+`
+
+  // Append elements to the modal
+  intensityModal.appendChild(modalImage)
+  intensityModal.appendChild(modalText)
+
+  // Append elements to the tooltip container
+  tooltipContainer.appendChild(helpSpan)
+  tooltipContainer.appendChild(intensityModal)
+
+  // Append tooltip container to the main container
+  mainContainer.appendChild(tooltipContainer)
+
+  // Append main container to the root element
+  rootElement.appendChild(mainContainer)
+}
