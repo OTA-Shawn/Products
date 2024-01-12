0b3c17b0beb77abce062e80cd35ad50cea72f7cc
OTA-Shawn <shawn.b@osakititan.com>
2024-01-12 16:39:33 -0600

    tw- prefix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const BRANDS = [
+  "Ador",
+  "Amamedic",
+  "Inada",
+  "Osaki",
+  "Otamic",
+  "Others",
+  "Theramedic",
+  "Titan",
+  "utils",
+]
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  // content: ["./**/*.{html,js}"],
+  content: [`./{${BRANDS.join(",")}}/**/*.{html,js}`],
   theme: {
     extend: {},
   },
